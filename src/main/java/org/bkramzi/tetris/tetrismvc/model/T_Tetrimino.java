64939274e007541bfe9a04bf10eedc1ffe5848da
ramzi bk <bk.ramzi@gmail.com>
2014-03-18 17:30:39 +0100

    Commit T type tetrimino

---
+++ src/main/java/org/bkramzi/tetris/tetrismvc/model/T_Tetrimino.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bkramzi.tetris.tetrismvc.model;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author ramzi
+ */
+public class T_Tetrimino extends Tetrimino{
+    
+    public T_Tetrimino(){
+        this.shape = new int[][][]{
+                        {{1,1,1},
+                         {0,1,0},
+                         {0,0,0}},
+                        //
+                        {{0,0,1},
+                         {0,1,1},
+                         {0,0,1}},
+                        //
+                        {{0,0,0},
+                         {0,1,0},
+                         {1,1,1}},
+                        //
+                        {{1,0,0},
+                         {1,1,0},
+                         {1,0,0}}};
+    
+        this.color =2;
+    }
+}
